23d84866ed1d1f8f4a2eda83111d0e9ee63ae142
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-30 23:33:59 +0200

    add example of tagged template strings

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+
+const fn = function(arr, arg1, arg2, arg3,) {
+    console.log(
+        arr,
+        arg1,
+        arg2,
+        arg3
+    )
+    return 'abc';
+}
+
+const v1 = 111;
+const v2 = 222;
+const text = fn`Ciąg znaków ${v1} oraz ${v2} i tyle!`;
+console.log(text); 
