894c7f7fcf90e683ed2ed0655e704f807f4c4e2c
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-31 00:09:09 +0200

    add usecase of spread operator

--- src/app.js
+++ src/app.js
@@ -13,3 +13,21 @@ const v1 = 111;
 const v2 = 222;
 const text = fn`Ciąg znaków ${v1} oraz ${v2} i tyle!`;
 console.log(text); 
+
+function useHTML(str, val) {
+    return `${str[0]} <b>${val}</b> ${str[1]}`
+}
+
+function useMarkDown(str, val) {
+    return `${str[0]} *${val}* ${str[1]}`
+}
+
+const name = "Lukasz";
+
+console.log(useHTML`Super programista ten ${name}!`, useMarkDown`Super programista ten ${name}`)
+
+const calcSum = (...nums) => {
+    return nums.reduce((acc, curr) => acc + curr, 0)
+}
+
+console.log(calcSum(1,2,3,4,5))
