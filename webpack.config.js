5f67645bf7f6dce8c4147f85bc7047596da44391
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-30 23:17:15 +0200

    update webpack.config.js with babel dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.js',
-    output:{
-        path: path.resolve(__dirname, 'build'),
-        fileName: 'app.min.js'
-    },
-    module: {
-        rules: []
-    }
-}
-
+  entry: './src/app.js',  // Assuming this is your entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),  // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,           // This tells Webpack to look for all .js files
+        exclude: /node_modules/, // Exclude files in the node_modules directory
+        use: {
+          loader: 'babel-loader', // Use babel-loader to transpile the files
+          options: {
+            presets: ['@babel/preset-env'] // Use the @babel/preset-env preset
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',  // Path to your HTML file
+      filename: 'index.html',
+    }),
+  ],
+};
